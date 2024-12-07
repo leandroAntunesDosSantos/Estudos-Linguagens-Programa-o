20bb6b69e4fa7312adae9924d556eb84211c6a47
leandroW <leandro_antunes28@hotmail.com>
2024-12-07 07:46:08 -0300

    laco de repeticao

---
+++ java/estudos-java/estrutura-dados-italents/lacos-repeticao/src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            //System.out.println(i);
+        }
+
+        int x = 1;
+
+        while (x < 10) {
+            //System.out.println(x);
+            x++;
+        }
+
+        int y = 1;
+        do {
+            System.out.println(y);
+            y++;
+        } while (y < 10);
+    }
+}
+
